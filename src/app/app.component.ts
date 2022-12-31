aa83d410be2e4fd58699e403fd1efeee9cb2ec26
Sivalatha800 <105136330+Sivalatha800@users.noreply.github.com>
2022-12-31 12:58:10 +0530

    5 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component, VERSION } from '@angular/core';
-
+declare function greetings(name: any): any;
+declare function test(): any;
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  ngOnInit(): void {
+    // call the externaljs functions
+    // greetings('rohol'); // with param
+    test();
+  }
 }
